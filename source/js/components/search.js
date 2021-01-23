f6cb5d3ed3d1ef107d7b2fd3aa7f0c73a609d358
Anton Kiyan <kiyananton@gmail.com>
2021-01-24 01:19:00 +0300

    initial-commit

---
+++ source/js/components/search.js
@@ -0,0 +1,50 @@
+(() => {
+  const items = document.querySelectorAll('.search');
+  const close = document.querySelectorAll('.search + .search-close');
+
+  if (!items[0]) {
+    return;
+  }
+
+  items.forEach((item) => {
+    item.addEventListener('focus', () =>{
+      item.parentNode.parentNode.classList.add('js-focus');
+    });
+  });
+
+  items.forEach((item) => {
+    item.addEventListener('blur', () => {
+      if (
+        item.value.split(' ').join('') === ''
+      ) {
+        item.value = '';
+      }
+
+      if (
+        item.value !== ''
+        && !item.classList.contains('js-inputed')
+      ) {
+        item.classList.add('js-inputed');
+      }
+
+      if (
+        item.value === ''
+        && item.classList.contains('js-inputed')
+      ) {
+        item.classList.remove('js-inputed');
+      }
+    })
+  });
+
+  close.forEach((item) => {
+    item.addEventListener('click', () => {
+      const search = item.previousElementSibling;
+      search.value = '';
+      if (
+        search.classList.contains('js-inputed')
+      ) {
+        search.classList.remove('js-inputed');
+      }
+    })
+  });
+})();
