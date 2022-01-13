3d36e5a0ed058c37c41219b17f4e01c1827f67c6
Anton <kiyananton@gmail.com>
2022-01-13 17:53:58 +0300

    Правки

--- build/js/script.js
+++ build/js/script.js
@@ -1047,7 +1047,7 @@
 })();
 
 (function () {
-  var contractsLinkAll = document.querySelectorAll('.contracts__link');
+  var contractsLinkAll = document.querySelectorAll(".contracts__link");
 
   if (!contractsLinkAll[0]) {
     return;
@@ -1056,13 +1056,13 @@
   var onLinkClick = function onLinkClick(e) {
     var target = e.target;
 
-    if (target.classList.contains('checkbox-btn__label') || target.classList.contains('checkbox-btn__checkbox') || target.classList.contains('contract-info__verification') || target.classList.contains('payment-waiting__confirm-btn') || target.classList.contains('contract-services__delivery-confirm')) {
+    if (target.classList.contains("checkbox-btn__label") || target.classList.contains("checkbox-btn__checkbox") || target.classList.contains("contract-info__verification") || target.classList.contains("payment-waiting__confirm-btn") || target.classList.contains("contract-services__delivery-confirm") || target.classList.contains("reports__download-btn") || $(target).parents(".reports__download-btn").length > 0 || target.classList.contains("reports__download-dropdown") || $(target).parents(".reports__download-dropdown").length > 0) {
       e.preventDefault();
     }
   };
 
   contractsLinkAll.forEach(function (link) {
-    link.addEventListener('click', onLinkClick);
+    link.addEventListener("click", onLinkClick);
   });
 })();
 
--- source/js/modules/contracts-all.js
+++ source/js/modules/contracts-all.js
@@ -1,25 +1,29 @@
-(function() {
-  const contractsLinkAll = document.querySelectorAll('.contracts__link');
+(function () {
+  const contractsLinkAll = document.querySelectorAll(".contracts__link");
 
   if (!contractsLinkAll[0]) {
     return;
   }
 
-  const onLinkClick = function(e) {
+  const onLinkClick = function (e) {
     const target = e.target;
 
     if (
-      target.classList.contains('checkbox-btn__label')
-      || target.classList.contains('checkbox-btn__checkbox')
-      || target.classList.contains('contract-info__verification')
-      || target.classList.contains('payment-waiting__confirm-btn')
-      || target.classList.contains('contract-services__delivery-confirm')
+      target.classList.contains("checkbox-btn__label") ||
+      target.classList.contains("checkbox-btn__checkbox") ||
+      target.classList.contains("contract-info__verification") ||
+      target.classList.contains("payment-waiting__confirm-btn") ||
+      target.classList.contains("contract-services__delivery-confirm") ||
+      target.classList.contains("reports__download-btn") ||
+      $(target).parents(".reports__download-btn").length > 0 ||
+      target.classList.contains("reports__download-dropdown") ||
+      $(target).parents(".reports__download-dropdown").length > 0
     ) {
       e.preventDefault();
     }
-  }
+  };
 
-  contractsLinkAll.forEach(function(link) {
-    link.addEventListener('click', onLinkClick);
+  contractsLinkAll.forEach(function (link) {
+    link.addEventListener("click", onLinkClick);
   });
 })();
